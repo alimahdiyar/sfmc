08d31e05ad71e54710d9191507b6e309adefaa1d
Ali Mahdiyar <alimahdiyar77@gmail.com>
2019-09-01 10:57:10 +0430

    Initial Commit

---
+++ sfmc/views.py
@@ -0,0 +1,8 @@
+from django.contrib.contenttypes.models import ContentType
+from django.http import Http404
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+
+def index_view(request):
+    return render(request, "index.html", {})
