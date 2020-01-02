ac011f4830367aaede8df1d79f9df9cadfe7a3fd
Admia1 <sampadpars@gmail.com>
2020-01-01 23:00:08 -0800

    adding some objects like team members file and ... BUT payment objects and maybe logicla delete remaining

--- competition/admin.py
+++ competition/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import CompetitionField, Manager, Participant, Team, PTR, UploadedFile
 # Register your models here.
+
+admin.site.register(CompetitionField)
+admin.site.register(Manager)
+admin.site.register(Participant)
+admin.site.register(Team)
+admin.site.register(PTR)
+admin.site.register(UploadedFile)
