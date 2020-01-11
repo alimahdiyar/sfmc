268ed9f7a8583fc61eb7a9ce867a13cbda140a7a
Ali Mahdiyar <alimahdiyar77@gmail.com>
2020-01-11 13:41:23 +0330

    registration form

--- competition/admin.py
+++ competition/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import CompetitionField, Participant, Team, UploadedFile
+from .models import CompetitionField, Participant, Team
 # Register your models here.
 
 admin.site.register(CompetitionField)
 admin.site.register(Participant)
 admin.site.register(Team)
-admin.site.register(UploadedFile)
--- competition/models.py
+++ competition/models.py
@@ -15,8 +15,8 @@ class TeamTypeConsts():
     )
 
 class CompetitionField(models.Model):
+    needs_advisor = models.BooleanField(default=True)
     name = models.CharField(max_length=100)
-    can_free_register = models.BooleanField(default=True)
     price = models.IntegerField()
     team_member_limit_min = models.IntegerField()
     team_member_limit_max = models.IntegerField()
@@ -26,29 +26,38 @@ class CompetitionField(models.Model):
 
 
 class Participant(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.PROTECT, related_name="team")
+
+    team = models.ForeignKey('Team', on_delete=models.CASCADE,
+                                related_name="members")
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    student_card_image = models.ImageField()
-    university = models.CharField(max_length=50)
+    student_card_image = models.ImageField(null=True, blank=True)
+    national_card_image = models.ImageField(null=True, blank=True)
+    university = models.CharField(max_length=255, null=True, blank=True)
+    organization = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name + " " + self.phone_number
 
+class Advisor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    university = models.CharField(max_length=255)
+
+
 
 class Team(models.Model):
+    advisor = models.OneToOneField(Advisor, null=True, blank=True, on_delete=models.PROTECT, related_name="team")
     manager = models.ForeignKey(Participant, on_delete=models.CASCADE,
                                 related_name="teams")  # redundant for development speed
     team_type = models.CharField(max_length=1, choices=TeamTypeConsts.states)
     name = models.CharField(max_length=100)
     competition_field = models.ForeignKey(CompetitionField, on_delete=models.CASCADE)
+    
+    upload_date = models.DateTimeField(null=True, blank=True)
+    uploaded_file = models.FileField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-
-class UploadedFile(models.Model):
-    team = models.ForeignKey(Team, on_delete=models.PROTECT)
-    upload_date = models.DateTimeField(auto_now_add=True)
-    uploaded_file = models.FileField()
