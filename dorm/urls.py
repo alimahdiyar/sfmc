8b76ec4fe6b676b64175a6b83ff5bd3c32cef952
Admia1 <sampadpars@gmail.com>
2020-02-06 16:24:36 -0800

    DBUG

--- dorm/urls.py
+++ dorm/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 app_name = "dorm"
 urlpatterns = [
     path('callback/', views.callback, name="callback"),
-    path('dorm_users/', views.dorm_users, name="payment"),
+    path('dorm_users/', views.dorm_users, name="dorm_users"),
     path('pay_bill/', views.pay_bill, name="pay_bill"),
 ]
