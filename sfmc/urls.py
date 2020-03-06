b211e6e6fcf22088d065e4b0a2322dd5e2bead9b
Ali Mahdiyar <alimahdiyar77@gmail.com>
2020-03-06 11:37:32 +0330

    close contest registration

--- sfmc/urls.py
+++ sfmc/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('contactus/', views.contactus_view, name='contactus'),
     path('regulations/', views.regulations_view, name='regulations'),
 
-    # path('register/', views.register_view, name='register'),
+    path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
