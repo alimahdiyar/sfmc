e7396247892e95b03bd5c111d65e608646c97605
Ali Mahdiyar <alimahdiyar77@gmail.com>
2020-03-13 10:14:12 +0330

    close contest registration

--- sfmc/views.py
+++ sfmc/views.py
@@ -27,109 +27,109 @@ def dashboard_view(request):
 
 
 def register_view(request):
-    # template = 'register_finished.html'
-    # if request.user.is_authenticated and not request.user.is_superuser:
-    #     return HttpResponseRedirect(reverse('dashboard'))
-    
-    # return render(request, template)
-    
-    template = 'register.html'
-    valid_team_types = [{'title': team_type[1], 'value': team_type[0]} for team_type in TeamTypeConsts.states]
-
-    if request.method == 'POST':
-        if User.objects.filter(username=request.POST['manager_email'].strip()).exists():
-            return render(request, template,
-                  {
-                      'competition_fields': CompetitionField.objects.all(),
-                      'valid_team_types': valid_team_types,
-                      'registration_error': 'ایمیل سرگروه از قبل وجود دارد',
-                      'form_data_json': request.POST['form_data_json']
-                  })
-        with transaction.atomic():
-            print(request.POST)
-            print(request.FILES)
-            
-            the_user = User.objects.create_user(
-                username=request.POST['manager_email'].strip(),
-                password=request.POST['manager_password_1'],
-            )
-
-            the_manager = Participant.objects.create(
-                user=the_user,
-                name=request.POST['manager_name'].strip(),
-                phone_number=request.POST['manager_phone_number'].strip(),
-                email=request.POST['manager_email'].strip(),
-            )
-
-            if request.POST['team_type'] == TeamTypeConsts.PARTICIPANT:
-
-                the_manager.university = request.POST['manager_university'].strip()
-                the_manager.student_card_image = request.FILES['manager_student_card_image']
-                the_manager.save()
-
-                members = {}
-                if request.POST['active_members']:
-                    for active_member in request.POST['active_members'].split(','):
-                        members[active_member] = Participant.objects.create(
-                            name=request.POST['member_' + active_member + '_name'].strip(),
-                            phone_number=request.POST['member_' + active_member + '_phone_number'].strip(),
-                            email=request.POST['member_' + active_member + '_email'].strip(),
-                            university=request.POST['member_' + active_member + '_university'].strip(),
-                            student_card_image=request.FILES['member_' + active_member + '_student_card_image']
-                        )
-
-                for competition_field in CompetitionField.objects.all():
-                    if 'competition_' + str(competition_field.pk) in request.POST:
-                        the_team = Team.objects.create(
-                            name=request.POST['competition_' + str(competition_field.pk) + '_team_name'],
-                            manager=the_manager,
-                            team_type=request.POST['team_type'],
-                            competition_field=competition_field
-                        )
-
-                        the_team.participants.add(the_manager)
-
-                        if competition_field.needs_adviser:
-                            the_adviser = Adviser.objects.create(
-                                name=request.POST['competition_' + str(competition_field.pk) + '_adviser_name'],
-                                email=request.POST['competition_' + str(competition_field.pk) + '_adviser_email'],
-                                university=request.POST[
-                                    'competition_' + str(competition_field.pk) + '_adviser_university'],
-                            )
-                            the_team.adviser = the_adviser
-                            the_team.save()
-
-                        for active_member in request.POST['active_members'].split(','):
-                            if 'member_' + active_member + '_competition_' + str(
-                                    competition_field.pk) in request.POST:
-                                the_team.participants.add(members[active_member])
-
-            else:
-
-                the_team = Team.objects.create(
-                    manager=the_manager,
-                    team_type=request.POST['team_type']
-                )
-
-                the_team.participants.add(the_manager)
-
-                if request.POST['team_type'] == TeamTypeConsts.PUBLIC_STUDENT:
-                    the_manager.university = request.POST['manager_university'].strip()
-                    the_manager.student_card_image = request.FILES['manager_student_card_image']
-                else:
-                    the_manager.organization = request.POST['manager_organization'].strip()
-                    the_manager.student_card_image = request.FILES['manager_national_card_image']
-
-                the_manager.save()
-
-            login(request, the_user)
+    template = 'register_finished.html'
+    if request.user.is_authenticated and not request.user.is_superuser:
         return HttpResponseRedirect(reverse('dashboard'))
-    return render(request, template,
-                  {
-                      'competition_fields': CompetitionField.objects.all(),
-                      'valid_team_types': valid_team_types,
-                      'registration_error': None
-                  })
+    
+    return render(request, template)
+    
+    # template = 'register.html'
+    # valid_team_types = [{'title': team_type[1], 'value': team_type[0]} for team_type in TeamTypeConsts.states]
+
+    # if request.method == 'POST':
+    #     if User.objects.filter(username=request.POST['manager_email'].strip()).exists():
+    #         return render(request, template,
+    #               {
+    #                   'competition_fields': CompetitionField.objects.all(),
+    #                   'valid_team_types': valid_team_types,
+    #                   'registration_error': 'ایمیل سرگروه از قبل وجود دارد',
+    #                   'form_data_json': request.POST['form_data_json']
+    #               })
+    #     with transaction.atomic():
+    #         print(request.POST)
+    #         print(request.FILES)
+            
+    #         the_user = User.objects.create_user(
+    #             username=request.POST['manager_email'].strip(),
+    #             password=request.POST['manager_password_1'],
+    #         )
+
+    #         the_manager = Participant.objects.create(
+    #             user=the_user,
+    #             name=request.POST['manager_name'].strip(),
+    #             phone_number=request.POST['manager_phone_number'].strip(),
+    #             email=request.POST['manager_email'].strip(),
+    #         )
+
+    #         if request.POST['team_type'] == TeamTypeConsts.PARTICIPANT:
+
+    #             the_manager.university = request.POST['manager_university'].strip()
+    #             the_manager.student_card_image = request.FILES['manager_student_card_image']
+    #             the_manager.save()
+
+    #             members = {}
+    #             if request.POST['active_members']:
+    #                 for active_member in request.POST['active_members'].split(','):
+    #                     members[active_member] = Participant.objects.create(
+    #                         name=request.POST['member_' + active_member + '_name'].strip(),
+    #                         phone_number=request.POST['member_' + active_member + '_phone_number'].strip(),
+    #                         email=request.POST['member_' + active_member + '_email'].strip(),
+    #                         university=request.POST['member_' + active_member + '_university'].strip(),
+    #                         student_card_image=request.FILES['member_' + active_member + '_student_card_image']
+    #                     )
+
+    #             for competition_field in CompetitionField.objects.all():
+    #                 if 'competition_' + str(competition_field.pk) in request.POST:
+    #                     the_team = Team.objects.create(
+    #                         name=request.POST['competition_' + str(competition_field.pk) + '_team_name'],
+    #                         manager=the_manager,
+    #                         team_type=request.POST['team_type'],
+    #                         competition_field=competition_field
+    #                     )
+
+    #                     the_team.participants.add(the_manager)
+
+    #                     if competition_field.needs_adviser:
+    #                         the_adviser = Adviser.objects.create(
+    #                             name=request.POST['competition_' + str(competition_field.pk) + '_adviser_name'],
+    #                             email=request.POST['competition_' + str(competition_field.pk) + '_adviser_email'],
+    #                             university=request.POST[
+    #                                 'competition_' + str(competition_field.pk) + '_adviser_university'],
+    #                         )
+    #                         the_team.adviser = the_adviser
+    #                         the_team.save()
+
+    #                     for active_member in request.POST['active_members'].split(','):
+    #                         if 'member_' + active_member + '_competition_' + str(
+    #                                 competition_field.pk) in request.POST:
+    #                             the_team.participants.add(members[active_member])
+
+    #         else:
+
+    #             the_team = Team.objects.create(
+    #                 manager=the_manager,
+    #                 team_type=request.POST['team_type']
+    #             )
+
+    #             the_team.participants.add(the_manager)
+
+    #             if request.POST['team_type'] == TeamTypeConsts.PUBLIC_STUDENT:
+    #                 the_manager.university = request.POST['manager_university'].strip()
+    #                 the_manager.student_card_image = request.FILES['manager_student_card_image']
+    #             else:
+    #                 the_manager.organization = request.POST['manager_organization'].strip()
+    #                 the_manager.student_card_image = request.FILES['manager_national_card_image']
+
+    #             the_manager.save()
+
+    #         login(request, the_user)
+    #     return HttpResponseRedirect(reverse('dashboard'))
+    # return render(request, template,
+    #               {
+    #                   'competition_fields': CompetitionField.objects.all(),
+    #                   'valid_team_types': valid_team_types,
+    #                   'registration_error': None
+    #               })
 
 
 def login_view(request):
